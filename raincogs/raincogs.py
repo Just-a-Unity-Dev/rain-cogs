f4cf0f2b5d3e1300f0e42ca9c961a09a67865a45
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-09-14 18:14:32 +0800

    deblessings

--- raincogs/raincogs.py
+++ raincogs/raincogs.py
@@ -30,8 +30,25 @@ class RainCogs(commands.Cog):
 	@config.command()
 	async def blessrole(self, ctx: commands.Context, blessRole) -> None:
 		"""set the bless role (role id)"""
+		if blessRole is None:
+			await self.config.guild(ctx.guild).blessrole.set(None)
+			return await ctx.reply("Reset the value of `blessrole` to `None`")
 		await self.config.guild(ctx.guild).blessrole.set(int(blessRole))
-		await ctx.reply(f"Set the value of `blessrole` to f{str(int(blessRole))}")
+		await ctx.reply(f"Set the value of `blessrole` to `{str(int(blessRole))}`")
+
+	async def blessing(self, ctx: commands.Context, target: discord.Member, role: bool):
+		guild = ctx.guild
+		blessed_role = await self.config.guild(guild).blessrole()
+		role = guild.get_role(blessed_role)
+		try:
+			if role:
+				await target.add_roles(role, reason="blessed")
+				return await ctx.send(f"blessed {target.mention()}")
+			else:
+				await target.remove_roles(role, reason="deblessed")
+				return await ctx.send(f"deblessed {target.mention()}")
+		except discord.errors.Forbidden:
+			return await ctx.send("the server admin didn't set up roles correctly, uh oh!\n\n*just so you know, this is not your fault. ping a server admin to fix this.*")
 
 	@rain.command()
 	@checks.admin_or_permissions(manage_roles=True)
@@ -42,17 +59,28 @@ class RainCogs(commands.Cog):
 		if not target:
 			return await ctx.send_help()
 		if ctx.me is target:
-			return await ctx.send("you cant bless me, i'm already blessed!")
+			return await ctx.send("you cant unbless me!")
 		if ctx.author is target:
-			return await ctx.send("you cant bless yourself!")
+			return await ctx.send("you cant unbless yourself!")
 
 		blessed_role = await self.config.guild(ctx.guild).blessrole()
 		if not blessed_role:
 			return await ctx.send("the server admin didn't set up bless roles correctly, uh oh!\n\n*just so you know, this is not your fault. ping a server admin to fix this.*")
-		guild = ctx.guild
-		role = guild.get_role(blessed_role)
-
-		try:
-			await target.add_roles(role, reason="blessed")
-		except discord.errors.Forbidden:
-			return await ctx.send("the server admin didn't set up roles correctly, uh oh!\n\n*just so you know, this is not your fault. ping a server admin to fix this.*")
+		self.blessing(ctx, target, True)
+	
+	@rain.command(aliases=["shadowrealm"])
+	@checks.admin_or_permissions(manage_roles=True)
+	async def unbless(self, ctx: commands.Context, target: discord.Member) -> None:
+		"""
+		deverifies a user
+		"""
+		if not target:
+			return await ctx.send_help()
+		if ctx.me is target:
+			return await ctx.send("you cant bless me, i'm already blessed!")
+		if ctx.author is target:
+			return await ctx.send("you cant bless yourself!")
+		blessed_role = await self.config.guild(ctx.guild).blessrole()
+		if not blessed_role:
+			return await ctx.send("the server admin didn't set up bless roles correctly, uh oh!\n\n*just so you know, this is not your fault")
+		self.blessing(ctx, target, False)
