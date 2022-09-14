3dde9e612e026ce58d16139d5993662b41498cd8
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-09-14 18:02:36 +0800

    fix more errors

--- raincogs/raincogs.py
+++ raincogs/raincogs.py
@@ -8,7 +8,7 @@ class RainCogs(commands.Cog):
 		self.bot = bot
 		self.config = Config.get_conf(self, 23975432657)
 		default_guild = {
-			"bless_role": None
+			"blessrole": None
 		}
 		self.config.register_guild(**default_guild)
 
@@ -30,8 +30,8 @@ class RainCogs(commands.Cog):
 	@config.command()
 	async def blessrole(self, ctx: commands.Context, blessRole) -> None:
 		"""set the bless role (role id)"""
-		await self.config.guild(ctx.guild)["bless_role"].set(int(blessRole))
-		await ctx.reply(f"Set the value of `bless_role` to f{str(int(blessRole))}")
+		await self.config.guild(ctx.guild).blessrole.set(int(blessRole))
+		await ctx.reply(f"Set the value of `blessrole` to f{str(int(blessRole))}")
 
 	@rain.command()
 	@checks.admin_or_permissions(manage_roles=True)
@@ -46,7 +46,7 @@ class RainCogs(commands.Cog):
 		if ctx.author is target:
 			return await ctx.send("you cant bless yourself!")
 
-		blessed_role = await self.config.guild(ctx.guild).bless_role()
+		blessed_role = await self.config.guild(ctx.guild).blessrole()
 		if not blessed_role:
 			return await ctx.send("the server admin didn't set up bless roles correctly, uh oh!\n\n*just so you know, this is not your fault. ping a server admin to fix this.*")
 		guild = ctx.guild
